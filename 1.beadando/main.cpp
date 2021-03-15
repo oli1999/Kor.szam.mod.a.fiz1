2c9841d88a8dc60a08cbe451957a068702a85ea4
Németh Olivér B <oli.bendeguz1999@gmail.com>
2021-03-15 17:54:13 +0100

    the working version

--- 1.beadando/main.cpp
+++ 1.beadando/main.cpp
@@ -7,24 +7,25 @@ using namespace std;
 // template azt csinálja hogy bármilyen 
 template<typename F> F fgv(F x){return x*x;} // ez a fgv
 template<typename D> D fgv_d(D x){return 2*x;} // ez a fgv deriváltja
-template<typename V, typename S> V newton_method(V x,V x2, V epsilon ,S f1 , S f2);
+template<typename V, typename S, typename S2> V newton_method(V x,V x2, V epsilon ,S f1 , S2 f2);
 
 int main() 
 {
-    float x , x2 , epsilon , dif;                       // egy random függvény érintőjének meredeksége (x1,f(x1)) pont és (x2,0 pont) között f fgv deriváltja
-    cout << "Kérem x1,x2 es epsilon erteket." << endl;
+    float x , x2 , epsilon ;                       // egy random függvény érintőjének meredeksége (x1,f(x1)) pont és (x2,0 pont) között f fgv deriváltja
+    cout << "Kerem x1,x2 es epsilon erteket." << endl;
     cin >> x >> x2 >> epsilon ;
-    float b = newton_method<float>(14,20,12,fgv,fgv_d);
+    float b = newton_method<float>(14,20,12,fgv<double>,fgv_d<double>);
+    cout <<  "Eredmeny: " << b << endl;
     return 0;
 }
 
-template<typename V , typename S> V newton_method(V x,V x2, V epsilon ,S f1 ,S f2) 
+template<typename V , typename S , typename S2> V newton_method(V x,V x2, V epsilon ,S f1 ,S2 f2) 
 {
-    dif = fgv(x) / fgv_d(x);                      // ez a sor azért kell h while el tudja kezdeni az összehasonlítást
-    while (abs(dif) >= epsilon)                   //epsilon mondja meg mikor vagyunk elég közel már zérushelyhez
+    V x1 = x - f1(x) / f2(x);                      // ez a sor azért kell h while el tudja kezdeni az összehasonlítást
+    while (abs(x1-x2) >= epsilon)                   //epsilon mondja meg mikor vagyunk elég közel már zérushelyhez
     {
-        dif = fgv(x) / fgv_d(x);
-        x2 = x2 - dif;                               // x2 = (x - fgv(x)) / fgv_d(x) ezt jelenti ez asor csak röviditve
+        x = x1;
+        x1 = x - f1(x) / f2(x);                               // x2 = (x - fgv(x)) / fgv_d(x) ezt jelenti ez asor csak röviditve
     }
     return x2;
 } 
