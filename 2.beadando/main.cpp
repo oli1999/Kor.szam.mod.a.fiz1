064f5ffe2ecf2391d5f545ef43b8ac8df5f2f59d
Németh Olivér B <oli.bendeguz1999@gmail.com>
2021-03-25 19:01:57 +0100

    I'm finised but it's not done.

--- 2.beadando/main.cpp
+++ 2.beadando/main.cpp
@@ -14,85 +14,35 @@
 
 
 
-// összeadás, értékadásösszeadás, skalárral való szorzás, kivonás, vektorok skaláris szorzata, normalizáció, vektorok hossza
-// vektorok skaláris szorzása ne legyen operator* jel, skalárral való szorzás legyen operator*
+
 #include <iostream>
+#include <cmath>
+#include "vector.h"
 using namespace std;
-
-template <typename T>
-struct Vector2d // definition of a struct, attention! it's not a class
-{
-    T x,y; // give some variables, in this case these are the coordinates
-                // of the 2 dim vector
-
-    // sum and value giving
-    Vector2d<T>& operator+= (Vector2d<T> const& v) //this is dif. from + op. 'cause ,this is not just a sum but also value giving at the same time
-                // that's why we have to initialize it inside the structure
-    {
-        x += v.x; y += v.y;
-        return *this; // return value points at the original objects
-    }
-    // multiplication with scalar v's left side
-    Vector2d<T>& operator*= (Vector2d<T> const& v , const double alfa) //this is dif. from * op. 'cause, this is not just a multiplication but also value giving at the same time
-                                        // that's why we have to initialize it inside the structure
-    {
-        x = alfa * v.x ; y = alfa * v.y;
-        return *this; // return value points at the original objects
-    }
-    // multiplication with scalar v's right side
-    Vector2d<T>& operator*= (Vector2d<T> const& v) //this is dif. from * op. 'cause, this is not just a multiplication but also value giving at the same time
-                                        // that's why we have to initialize it inside the structure
-    {
-        x = v.x * alfa ; y = v.y * alfa;
-        return *this; // return value points at the original objects
-    }
-};
-
-template <typename S>
-Vector2d<S> operator+ (Vector2d<S> const& a, Vector2d<S> const& b) // definition of the sum operation
-                // when you call this sum you have to give it 2 vec with 2 comp.
-                // in the definition only the binary loc. was given to the def.
-                // 'cause if it is a 100 dim instead of two you would run out of ram
-{
-    return Vector2d<S>{a.x + b.x , a.y + b.y}; // this will give you back the sum of the
-                                           // 2 vector's comp. separated
-};
-
-template <typename M>   // multiplication a's right side
-Vector2d<M> operator* (Vector2d<M> const& a, Vector2d<M> const& b) 
-{
-    return Vector2d<M>{a.x * b.x , a.y * b.y}; 
-};
-
-template <typename MM>      // multiplication a's left side
-Vector2d<MM> operator* (Vector2d<MM> const& a, Vector2d<MM> const& b)
-{
-    return Vector2d<MM>{b.x * a.x , b.y * a.y};
-};
-
-template <typename SM>      // multiplication with scalar
-Vector2d<SM> operator* (Vector2d<SM> const& a , Vector2d<SM> double alfa)
-{
-    return Vector2d<SM>{alfa * a.x , alfa * a.y}; // It gives back vectors 2 comp. multi. with the same scalar
-};
-
-
-
-
-
-
 
 
 
 int main() 
 {
-    Vector2d<double> v = {17.0 , 2.0}; // def. and value giving
-    Vector2d<double> u = {4.0 , -2.0}; // def. and value giving
-    auto w = v * u;                     
-    std::cout << v.x << " " << v.y << endl;
-    std::cout << u.x << " " << u.y << endl;
-    std::cout << w.x << " " << w.y << endl;
-    auto p = u * v;
-    std::cout << p.x << " " << p.y << endl;
+    Vector2d<double> v = {17.0 , 2.0};                                                // def. and value giving
+    Vector2d<double> u = {4.0 , -2.0};                                                // def. and value giving
+    std::cout << v.x << " " << v.y << endl;                                         
+    std::cout << u.x << " " << u.y << endl;                                         
+    auto q = v + u;
+    std::cout << q.x << " " << q.y << endl;                                          
+    auto r = u + v;
+    std::cout << r.x << " " << r.y << endl;                                         
+    v += u;
+    std::cout << v.x << " " << v.y << endl;                                             
+    std::cout << u.x << " " << u.y << endl;                                               
+    Vector2d<double> v2 = {17.0 , 2.0};                                                // def. and value giving
+    Vector2d<double> u2 = {4.0 , 2.0};                                                // def. and value giving
+    auto w = v2 * u2;                     
+    std::cout << w.x << " " << w.y << endl;                                             
+    auto k = length_of_vec(u2);
+    std::cout << k.x << " " << k.y << endl;                                         // I'm not sure, this is the task, but it's workin'
+    auto ww = v2 - u2;                                                               
+    std::cout << ww.x << " " << ww.y << endl; 
+
     return 0;
 }
--- 2.beadando/vector.h
+++ 2.beadando/vector.h
@@ -1,82 +1,60 @@
 template <typename T>
-struct Vector2d // definition of a struct, attention! it's not a class
+struct Vector2d                                                             // definition of a struct, attention! it's not a class
 {
-    T x,y; // give some variables, in this case these are the coordinates
-                // of the 2 dim vector
+    T x,y;                                                                  // give some variables, in this case these are the coordinates of the 2 dim vector
 
     // sum and value giving
-    Vector2d<T>& operator+= (Vector2d<T> const& v) //this is dif. from + op. 'cause ,this is not just a sum but also value giving at the same time
-                // that's why we have to initialize it inside the structure
+    Vector2d<T>& operator+= (Vector2d<T> const& v)                          //this is dif. from + op. 'cause ,this is not just a sum but also value giving at the same time
+                                                                            // that's why we have to initialize it inside the structure
     {
         x += v.x; y += v.y;
-        return *this; // return value points at the original objects
-    }
-    // multiplication a's right side
-    Vector2d<T>& operator*= (Vector2d<T> const& v , double alfa) //this is dif. from * op. 'cause, this is not just a multiplication but also value giving at the same time
-                                        // that's why we have to initialize it inside the structure
-    {
-        x = alfa * v.x ; y = alfa * v.y;
-        return *this; // return value points at the original objects
-    }
-    // multiplication a's left side
-    Vector2d<T>& operator*= (Vector2d<T> const& v) //this is dif. from * op. 'cause, this is not just a multiplication but also value giving at the same time
-                                        // that's why we have to initialize it inside the structure
-    {
-        x *= v.x ; y *= v.y;
-        return *this; // return value points at the original objects
+        return *this;                                                       // return value points at the original objects
     }
 
-    // substraction and value giving
-    Vector2d<T>& operator-= (Vector2d<T> const& v) //this is dif. from * op. 'cause, this is not just a multiplication but also value giving at the same time
-                                        // that's why we have to initialize it inside the structure
-    {
-        // magic happens here
-        return *this; // return value points at the original objects
-    }
+    
 };
 
+// addition
 template <typename S>
-Vector2d<S> operator+ (Vector2d<S> const& a, Vector2d<S> const& b) // definition of the sum operation
-                // when you call this sum you have to give it 2 vec with 2 comp.
-                // in the definition only the binary loc. was given to the def.
-                // 'cause if it is a 100 dim instead of two you would run out of ram
+Vector2d<S> operator+ (Vector2d<S> const& a, Vector2d<S> const& b)          // when you call this sum you have to give it 2 vec with 2 comp. in the definition only the binary loc. was given to the def.
+                                                                            // 'cause if it is a 100 dim instead of two you would run out of ram
 {
-    return Vector2d<S>{a.x + b.x , a.y + b.y}; // this will give you back the sum of the
-                                           // 2 vector's comp. separated
+    return Vector2d<S>{a.x + b.x , a.y + b.y};                             // this will give you back the sum of the 2 vector's comp. separated
 };
 
-template <typename M>   // multiplication a's right side
+// substraction
+template <typename SS>
+Vector2d<SS> operator- (Vector2d<SS> const& a, Vector2d<SS> const& b)          // when you call this sub you have to give it 2 vec with 2 comp. in the definition only the binary loc. was given to the def.
+                                                                            // 'cause if it is a 100 dim instead of two you would run out of ram
+{
+    return Vector2d<SS>{a.x - b.x , a.y - b.y};                             // this will give you back the substraction of the 2 vector's comp. separated
+};
+
+
+//multiplication
+template <typename M>   
 Vector2d<M> operator* (Vector2d<M> const& a, Vector2d<M> const& b) 
 {
-    return Vector2d<M>{a.x * b.x , a.y * b.y}; 
+    return Vector2d<M>{a.x * b.x , a.y * b.y};                                          // this mult. the 2 vector's x and y comp. with eachother
 };
 
-template <typename MM>      // multiplication a's left side
-Vector2d<MM> operator* (Vector2d<MM> const& a, Vector2d<MM> const& b)
-{
-    return Vector2d<MM>{b.x * a.x , b.y * a.y};
-};
+// multiplication2
+//template <typename MM>      
+//Vector2d<MM> operator* (Vector2d<MM> const& a, Vector2d<MM> const& b)
+//{
+//    return Vector2d<MM>{b.x * a.x , b.y * a.y};
+//};
 
-template <typename SM>      // multiplication with scalar
-Vector2d<SM> operator* (Vector2d<SM> const& a , Vector2d<SM> const& alfa)
-{
-    return Vector2d<SM>{alfa * a.x , alfa * a.y}; // It gives back vectors 2 comp. multi. with the same scalar
-};
+// multiplication with scalar
+//template <typename SM>      
+//Vector2d<SM> operator* (Vector2d<SM> const& a , const double alfa)
+//{
+//    return Vector2d<SM>{alfa * a.x , alfa * a.y};                               // It gives back vectors 2 comp. multi. with the same scalar
+//};
 
-template <typename SU>      // substraction
-Vector2d<SU> operator- (Vector2d<SU> const& a , Vector2d<SU> const& b)
+//length
+template <typename L>   
+Vector2d<L> length_of_vec (Vector2d<L> const& a)                                
 {
-    return Vector2d<SU>{ a.x - b.x ,  a.y - b.y}; 
-};
-
-template <typename NO>      // normalization
-Vector2d<>  (Vector2d<> const& a , Vector2d<> const& b)
-{
-    return Vector2d<>{}; 
-};
-
-template <typename LE>      // length
-Vector2d<>  (Vector2d<> const& a , Vector2d<> const& b)
-{
-    return Vector2d<>{}; 
+    return Vector2d<L>{sqrt(a.x * a.x + a.y * a.y)};                            // add the vectors 2 comp. square, and after that it takes the square root of the sum
 };
