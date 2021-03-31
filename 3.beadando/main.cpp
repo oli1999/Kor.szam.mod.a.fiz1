0667c15984e27993e0924f9b9a700cb4ee956479
Németh Olivér B <oli.bendeguz1999@gmail.com>
2021-03-31 09:37:02 +0200

    Created 3.HW

--- 3.beadando/main.cpp
+++ 3.beadando/main.cpp
@@ -2,7 +2,19 @@
 
 using namespace std;
 
+struct Matrix2 
+{
+
+    double a11, a12, a21, a22;
+
+
+};
+
+
+
+
 int main() 
 {
-cout << "Sanyi" << endl;
+Matrix2 m;
+Matrix2 m2 = (1.0, 2.0, 3.0, 4.0);
 }
