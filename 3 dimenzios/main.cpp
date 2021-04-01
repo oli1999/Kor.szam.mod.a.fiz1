837fbd416147279b9ddbc6d731ba242ab5eadb0e
Németh Olivér B <oli.bendeguz1999@gmail.com>
2021-04-01 19:39:30 +0200

    modify big project

--- 3 dimenzios/main.cpp
+++ 3 dimenzios/main.cpp
@@ -1,16 +1,9 @@
 #include <iostream>
-#include <iomanip>
+using namespace std;
 
-int main(int, char**) 
+int main() 
 {
-    int fahr;
-    for ( fahr = -100; fahr <= 400; fahr += 25)
-    {
-        // printf("Fahr  = %d\t Cels = %f\n ", fahr, 5./9. * (fahr-32));
-        std::cout << "fahr = " << std::setw(5) << fahr
-                  << ", cels = " << std::setw(8)  << 5./9. * (fahr-32)
-                  << std::endl;
-    }
+    cout << "Sanyi" << endl;
     return 0;
 }
 
