e9f0d3bad3ea49f49a1ba262049891f6f19dd80d
Németh Olivér B <oli.bendeguz1999@gmail.com>
2021-04-07 22:12:26 +0200

    minden javítva , beolvasás kiírás nincs még

--- 2.beadando/main.cpp
+++ 2.beadando/main.cpp
@@ -26,31 +26,36 @@ int main()
 {
     Vector2d<double> v = {17.0 , 2.0};                                                // def. and value giving
     Vector2d<double> u = {4.0 , -2.0};                                                // def. and value giving
-    std::cout << "v vector: " << v.x << " " << v.y << endl;                                         
+    std::cout << "v vector: " << v.x << " " << v.y << endl;                                        
     std::cout << "u vector: "<< u.x << " " << u.y << endl;                                         
-    auto q = v + u;
+    auto q = v + u;                                                // sima összeadás
     std::cout << "q vector: "<< q.x << " " << q.y << endl;                                          
-    auto r = u + v;
+    auto r = u + v;                                               //fordított sorrendű összeadás
     std::cout << "r vector: "<< r.x << " " << r.y << endl;                                         
-    v += u;
+    v += u;                                                       // összeadás és értékadás
     std::cout << "v vector: "<< v.x << " " << v.y << endl;                                             
     std::cout << "u vector: "<< u.x << " " << u.y << endl;                                               
     Vector2d<double> v2 = {17.0 , 2.0};                                                // def. and value giving
     Vector2d<double> u2 = {4.0 , 2.0};                                                // def. and value giving
     std::cout << "v2 vector: "<< v2.x << " " << v2.y << endl;                                             
     std::cout << "u2 vector: "<< u2.x << " " << u2.y << endl; 
-    v2 -= u2;
+    v2 -= u2;                                                       // kivonás és értékadás
     std::cout << "v2 vector: "<< v2.x << " " << v2.y << endl;                                             
-    std::cout << "u2 vector: "<< u2.x << " " << u2.y << endl;                                             
-    //auto w = v2 * u2;                     
-    //std::cout << "w vector: "<< w.x << " " << w.y << endl;                                             
+    std::cout << "u2 vector: "<< u2.x << " " << u2.y << endl;                                                                                        
     double constans = 5.0;
-    auto c_u2 = constans * u2;
+    auto c_u2 = constans * u2;                                      // skalárral való szorzás
     std::cout << "c_u2 vector: "<< c_u2.x << " " << c_u2.y << endl;
-    auto k = length_of_vec(u2);
-    std::cout << "k vector: "<< k.x << " " << k.y << endl;                                         // I'm not sure, this is the task, but it's workin'
-    auto ww = v2 - u2;                                                               
+    auto k = length_of_vec(u2);                                     // vektor hossza
+    std::cout << "k vector: "<< k.x  << endl;                                         
+    auto ww = v2 - u2;                                              // sima kivonás                 
     std::cout << "ww vector: "<< ww.x << " " << ww.y << endl; 
+    Vector2d<double> v3 = {10.0 , 20.0};                                                // def. and value giving
+    Vector2d<double> u3 = {15.0 , 25.0};                                                // def. and value giving
+    auto www = dot_product(v3 , u3);                                // skaláris szorzás
+    std::cout << "ww vector: "<< www << endl; 
+    v3 *= constans;
+    std::cout << "v3 vector: "<< v3.x << " " << v3.y << endl;
+
 
     return 0;
 }
--- 2.beadando/vector.h
+++ 2.beadando/vector.h
@@ -18,6 +18,15 @@ struct Vector2d                                                             // d
         x -= v.x; y -= v.y;
         return *this;                                                       // return value points at the original objects
     }
+
+    // multiplication with scalar and value giving
+    Vector2d<T>& operator*= ( T alfa)                                      //this is dif. from * op. 'cause ,this is not just a  multiplication with scalar but also value giving at the same time
+                                                                            // that's why we have to initialize it inside the structure
+    {
+        
+        x *= alfa ; y *= alfa ;
+        return *this;                                                       // return value points at the original objects
+    }
     
 };
 
@@ -40,16 +49,16 @@ Vector2d<SS> operator- (Vector2d<SS> const& a, Vector2d<SS> const& b)          /
 
 //dot product
 template <typename DM>   
-Vector2d<DM> ˙ (Vector2d<DM> const& a, Vector2d<DM> const& b) 
+DM dot_product (Vector2d<DM> const& a, Vector2d<DM> const& b) 
 {
-    return a.x * b.x + a.y * b.y;                                          // this mult. the 2 vector's x and y comp. with eachother
+    return  (a.x * b.x) + (a.y * b.y) ;                                          // this mult. the 2 vector's x and y comp. with eachother
 };
 
 
 
 // multiplication with scalar
 template <typename SM>      
-Vector2d<SM> operator* (Vector2d<SM> const& a , const double alfa)
+Vector2d<SM> operator* (const double alfa , Vector2d<SM> const& a)
 {
     return Vector2d<SM>{alfa * a.x , alfa * a.y};                               // It gives back vectors 2 comp. multi. with the same scalar
 };
@@ -61,4 +70,11 @@ template <typename L>
 Vector2d<L> length_of_vec (Vector2d<L> const& a)                                
 {
     return Vector2d<L>{sqrt(a.x * a.x + a.y * a.y)};                            // add the vectors 2 comp. square, and after that it takes the square root of the sum
+};
+
+//cin
+template <typename CIN>   
+Vector2d<CIN> vec_cin (Vector2d<CIN> const& a)                                
+{
+    return Vector2d<CIN> cin << a.x << a.y ;                            // add the vectors 2 comp. square, and after that it takes the square root of the sum
 };
