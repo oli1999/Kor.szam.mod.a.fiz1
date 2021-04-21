ea4856567c7825e252cec4faffafe53495807cfa
Németh Olivér B <oli.bendeguz1999@gmail.com>
2021-04-21 17:21:33 +0200

    working with Newton-Cotes

--- 3.beadando/main.cpp
+++ 3.beadando/main.cpp
@@ -1,7 +1,7 @@
-//#include <iostream>
+#include <iostream>
 //#include <arc_length.h>
-//using namespace std;
-//#include <cmath>
+using namespace std;
+#include <cmath>
 
 // csináljunk egy osztályt, aminek az egyes függvényei az integrálás meg deriválás
 // pontosabban  - ívhossz szerinti integrál
@@ -14,35 +14,15 @@
 
 // AZ ÍVHOSSZ SZERINTI INTEGRÁL: integrate(a,b)  (1 + f'^2)^(1/2)  ds
 
-template <typename T>
-double Richardson_ex(int x, double dx, T f, const int F, const double q, const int a, const int h)
-{
-    return (5*f(x + 4*dx , ) - 200*f(x + 2*dx) + 1280*f(x + dx) - 1280*f(x - dx) + 200*f(x - 2*dx) -5*f(x - 4*dx));
-}
 
 
-double Newton_Cotes(T f, double dx, const int n)
-{
-    double M;
-    for (int i = 0; i < n; i++)
-    {
-        M += f(m[i]) * dx;
-    }
-    
-}
-
-double trapezoidal(T f, double dx, const int n)
-{
-    // ugyanaz mint a Newton_Cotes csak le kell vonni f(x0) / 2 és f(xn) / 2 a végén
-}
-
-
-
+//HÁZIBAN MEGADOTT FGV
 double func (double x , const int F , const double q , const int a , const int h)
 {
     return ((F/q) * (cosh((q*x)/F) - cosh((q*a)/(2*F)) + h);
 }
 
+//INTEGRÁLOKHOZ KELL EGYENLŐ LÉPÉSKÖZ
 double middle ( int x, int x1, const int n ) // itt az x kezdőérték szerepébe bújik, onnan számolva 
 {
     double m[n];
@@ -50,15 +30,42 @@ double middle ( int x, int x1, const int n ) // itt az x kezdőérték szerepéb
     {
         double something;
         something = [(x - x1) / 2] + i*(x - x1) ;
-        m.push_back(something)
+        m.push_back(something);
     }
     return m;
 }
 
 
+//RICHARDSON EXTRAPOLÁCIÓ 6. RENDŰ KÖZELÍTÉS
+template <typename T>
+double Richardson_ex(int x, double dx, T f, const int F, const double q, const int a, const int h)
+{
+    return (5*f(x + 4*dx , ) - 200*f(x + 2*dx) + 1280*f(x + dx) - 1280*f(x - dx) + 200*f(x - 2*dx) -5*f(x - 4*dx));
+}
 
+// NEWTON-COTES FORMULA
+double Newton_Cotes(T f, int m, const int n)
+{
+    double M;
+    double step = [m[1] - m[0]];
+    for (int i = 0; i < n; i++)
+    {
+        M += f(m[i]) * step;
+    }
+    return M;
+}
 
-
-
-
+//TRAPÉZOS GÖRBE ALATTI TERÜLET
+double trapezoidal(T f, int m, const int n)
+{
+    // ugyanaz mint a Newton_Cotes csak le kell vonni f(x0) / 2 és f(xn) / 2 a végén
+    double MM;
+    double step = [m[1] - m[0]];
+    for (int i = 0; i < n; i++)
+    {
+        MM += f(m[i]) * step;
+    }
+    MM = MM - [f(m[0]) / 2] - [f(m[n]) / 2];
+    return MM;
+}
 
